26bb2dda8a4d77d96cb0dc89fe275d52f033a00a
Amdrii <andreyk557@gmail.com>
2022-07-09 21:07:44 +0300

    start

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:0,
+        nav:false,
+        dots:false,
+        pause: 'false',
+        navText: [ '', ' ' ],
+        autoplay: false,
+        autoplayTimeout: 5000,
+        autoplaySpeed: 1500,
+        autoplayHoverPause: 0,
+        navSpeed: 3000,
+        responsive:{
+             0:{
+                items:1
+            }
+        }
+    });
+});
